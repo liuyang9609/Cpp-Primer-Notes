fa49ef292cfc579086ee8b0e6d9e3e597ef4dc3d
liuyang9609 <ly_0913@foxmail.com>
2020-08-02 15:56:05 +0800

    Create ex4_10.cpp

---
+++ ch04/ex4_10.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> num;
+    int i;
+    while (cin >> i && i != 42) {
+        num.push_back(i);
+    }
+
+    for (auto j : num) {
+        cout << j << endl;
+    }
+
+    return 0;
+}
